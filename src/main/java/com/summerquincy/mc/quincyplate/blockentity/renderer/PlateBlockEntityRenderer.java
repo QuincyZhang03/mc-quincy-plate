72af46bd32f1ebc081a0310b65c596d27bfb17bf
QuincyZhang03 <1391070463@qq.com>
2026-05-31 19:41:22 +0800

    release 1.0.3

--- src/main/java/com/summerquincy/mc/quincyplate/blockentity/renderer/PlateBlockEntityRenderer.java
+++ src/main/java/com/summerquincy/mc/quincyplate/blockentity/renderer/PlateBlockEntityRenderer.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 import com.summerquincy.mc.quincyplate.blockentity.PlateBlockEntity;
 import com.summerquincy.mc.quincyplate.blockentity.data.PlateContentItem;
+import com.summerquincy.mc.quincyplate.item.ModItems;
 import com.summerquincy.mc.quincyplate.util.DistanceHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LightTexture;
@@ -14,6 +15,7 @@ import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LightLayer;
 import net.minecraftforge.api.distmarker.Dist;
@@ -43,32 +45,44 @@ public class PlateBlockEntityRenderer implements BlockEntityRenderer<PlateBlockE
             item.initLayer();
             double x = item.getPosX();
             double z = item.getPosZ();
+            ItemStack itemStack = item.getItem();
             for (int j = 0; j < i; j++) {
                 //搜索已经渲染的物品，在待渲染位置附近的叠在高度上
                 //最终渲染层数=附近最高的物品层数+1
                 PlateContentItem prevItem = foodList.get(j);
                 if (DistanceHelper.isDistanceWithinScope(x, z, prevItem.getPosX(), prevItem.getPosZ(),
-                        2 * PlateBlockEntity.SELECTION_TOLERANCE)) {
+                        item.getItem().is(ModItems.FORK.get()) ?
+                                PlateBlockEntity.FORK_ON_FOOD_DISTANCE : 2 * PlateBlockEntity.SELECTION_TOLERANCE))
+                {
                     item.ensureStackOn(prevItem);
                 }
             }
             poseStack.pushPose();
-            poseStack.translate(x, BASE_HEIGHT + LAYER_HEIGHT * item.getRenderLayer(), z);
-            poseStack.scale(ITEM_SIZE, THICKNESS, ITEM_SIZE);
             /*
-            对物品而言，厚度是y轴，很奇怪
-                ↑ y（厚度）
+            对物品而言，
+                ↑ y
                 |
                 ·——→ x
-               / 物品平面(xOz)
+               /
               z
+              但注意平移、旋转和缩放的三个矩阵是独立的，
+              旋转坐标轴对缩放使用的坐标轴没有影响。
             */
-            poseStack.mulPose(Axis.YP.rotation((float) item.getRotation()));
-            //先绕Y轴旋转，这时元素自身的X轴也跟着旋转了，再绕自身X轴旋转就可以躺在盘子上了
-            poseStack.mulPose(Axis.XP.rotationDegrees(90));
+            if (itemStack.is(ModItems.FORK.get())) {
+                poseStack.translate(x, BASE_HEIGHT + LAYER_HEIGHT * item.getRenderLayer() + ITEM_SIZE * 0.4, z);
+                poseStack.scale(ITEM_SIZE, ITEM_SIZE, ITEM_SIZE);
+                poseStack.mulPose(Axis.YN.rotation((float) item.getRotation()));
+                poseStack.mulPose(Axis.ZN.rotationDegrees(135));
+            } else {
+                poseStack.translate(x, BASE_HEIGHT + LAYER_HEIGHT * item.getRenderLayer(), z);
+                poseStack.scale(ITEM_SIZE, THICKNESS, ITEM_SIZE);
+                //先绕Y轴旋转，这时元素自身的X轴也跟着旋转了，再绕自身X轴旋转就可以躺在盘子上了
+                poseStack.mulPose(Axis.YP.rotation((float) item.getRotation()));
+                poseStack.mulPose(Axis.XP.rotationDegrees(90));
+            }
             Level level = blockEntity.getLevel();
             if (level != null) {
-                renderer.renderStatic(item.getItem(), ItemDisplayContext.FIXED,
+                renderer.renderStatic(itemStack, ItemDisplayContext.FIXED,
                         getLightLevel(blockEntity.getLevel(), blockEntity.getBlockPos()),
                         OverlayTexture.NO_OVERLAY, poseStack, buffer, blockEntity.getLevel(), 0
                 );
--- src/main/java/com/summerquincy/mc/quincyplate/item/ForkItem.java
+++ src/main/java/com/summerquincy/mc/quincyplate/item/ForkItem.java
@@ -2,15 +2,24 @@ package com.summerquincy.mc.quincyplate.item;
 
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
+import com.summerquincy.mc.quincyplate.blockentity.PlateBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -18,6 +27,10 @@ import java.util.List;
 public class ForkItem extends Item {
     public ForkItem(Properties pProperties) {
         super(pProperties);
+    }
+
+    public ForkItem() {
+        this(new Item.Properties().stacksTo(1));
     }
 
     @SuppressWarnings({"deprecation", "NullableProblems"})
@@ -34,5 +47,35 @@ public class ForkItem extends Item {
     public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
         super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
         pTooltipComponents.add(Component.translatable("tooltip.quincyplate.fork_common"));
+    }
+
+    //Shift+右键时，不触发方块的use方法，而是对着方块使用物品的useOn方法
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        Level level = pContext.getLevel();
+        if (!level.isClientSide) {
+            BlockPos platePos = pContext.getClickedPos();
+            Vec3 hit = pContext.getClickLocation();
+            BlockEntity blockEntity = level.getBlockEntity(platePos);
+            if (blockEntity instanceof PlateBlockEntity plate &&
+                    pContext.getHand() == InteractionHand.MAIN_HAND &&
+                    pContext.getClickedFace() == Direction.UP
+            ) {
+                ItemStack forkStack = pContext.getItemInHand();
+                Player player = pContext.getPlayer();
+                double x = hit.x - plate.getBlockPos().getX();
+                double z = hit.z - plate.getBlockPos().getZ();
+                double rotation = Math.toRadians(pContext.getRotation());
+                if (plate.stickFork(player, forkStack.copyWithCount(1), x, z, rotation)) {
+                    if (player!=null && !player.getAbilities().instabuild) {
+                        forkStack.shrink(1);
+                    }
+                    return InteractionResult.SUCCESS;
+                }
+                return InteractionResult.PASS;
+            }
+        }
+        return super.useOn(pContext);
     }
 }
